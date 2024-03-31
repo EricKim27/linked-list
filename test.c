0eed96dcc69d37b83478ef5f5f472fb705dfb031
EricKim27 <seohyun.kim@outlook.kr>
2024-03-31 18:22:29 +0900

    added sigint

--- test.c
+++ test.c
@@ -6,6 +6,14 @@
  * It creates a list then prints the value. 
  * based on the number provided in the arguments, it creates that amount of entries.
  */
+NODE_HEADER *test;
+void handle_sigint(int sig)
+{
+    printf("Interrupted\n");
+    flush_list(test);
+    exit(EXIT_FAILURE);
+}
+
 int main(int argc, char *argv[])
 {
     if(argc < 2)
@@ -16,7 +24,7 @@ int main(int argc, char *argv[])
     {
         char *name = "example";
         char *data = "example data";
-        NODE_HEADER *test = init_node(name, data);
+        test = init_node(name, data);
         NODE *prev_node = test->next;
         NODE *cursor;
         for(int i = 0; i<atoi(argv[1]); i++)
