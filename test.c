8ee018dacb34e78496aa385be918749a299ef902
EricKim27 <seohyun.kim@outlook.kr>
2024-04-02 11:49:18 +0900

    fixed sigint

--- test.c
+++ test.c
@@ -1,21 +1,27 @@
 #include "linked.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#define errExit(msg)    \
+  do {                  \
+    perror(msg);        \
+    exit(EXIT_FAILURE); \
+  } while (0)
 /*
  * This code is for testing the linked list library that I made.
  * It creates a list then prints the value. 
  * based on the number provided in the arguments, it creates that amount of entries.
  */
 NODE_HEADER *test;
-void handle_sigint(int sig)
-{
-    printf("Interrupted\n");
-    flush_list(test);
-    exit(EXIT_FAILURE);
+
+static void sigintHandler(int sig) {
+  write(STDERR_FILENO, "Caught SIGINT!\n", 15);
 }
 
 int main(int argc, char *argv[])
 {
+    if(signal(SIGINT, sigintHandler) == SIG_ERR) errExit("interrupted!"); 
     if(argc < 2)
     {
         printf("usage: %s <number of lists>\n", argv[0]);
